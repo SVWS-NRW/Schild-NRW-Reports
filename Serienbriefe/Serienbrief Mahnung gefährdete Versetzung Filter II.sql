d86dde1ca84f9c05fbaf0ac1cc04195154cee5b3
Raffenberg <fb.raffenberg@gmail.com>
2024-03-18 10:39:46 +0100

    Bili-Bescheinigungen SI und SII, Mahnreport Filter II
    Die Bili-Bescheinigungen für die SI und SII wurden finalisiert
    Die Anleitung für die Serienbriefe wurden um eine Beschreibung zum neuen Mahnbrief und die Möglichkeit der Einbindung der integrierten Platzhalter des Reportdesignes ergänzt
    Der Mahnbrief für Filter II wurde hinzugefügt. Dieser benutzt eine andere Unterdatenquelle.

--- Serienbriefe/Serienbrief Mahnung gefährdete Versetzung Filter II.sql
+++ Serienbriefe/Serienbrief Mahnung gefährdete Versetzung Filter II.sql
@@ -1,13 +1,13 @@
 SELECT Schueler.* FROM Schueler
 WHERE Schueler.Status IN (2)
 AND Schueler.Geloescht='-'
-AND Schueler.ASDJahrgang<>'05'
+AND Schueler.ASDJahrgang IN ('06','07','08','09','10','EF')
 AND EXISTS (SELECT slad.ID FROM SchuelerLernabschnittsdaten AS slad
-LEFT JOIN SchuelerLeistungsdaten AS sld ON sld.Abschnitt_ID=slad.ID
-WHERE slad.Schueler_ID=Schueler.ID
-AND slad.Jahr=(SELECT sch.Schuljahr FROM EigeneSchule AS sch)
-AND slad.Abschnitt=(SELECT sch.SchuljahrAbschnitt FROM EigeneSchule AS sch) 
-AND slad.WechselNr=999 
-AND slad.Hochrechnung=0
-AND sld.Warnung='+'
-AND sld.Warndatum IS NULL)
+	LEFT JOIN SchuelerLeistungsdaten AS sld ON sld.Abschnitt_ID=slad.ID
+	WHERE slad.Schueler_ID=Schueler.ID
+	AND slad.Jahr=(SELECT sch.Schuljahr FROM EigeneSchule AS sch)
+	AND slad.Abschnitt=(SELECT sch.SchuljahrAbschnitt FROM EigeneSchule AS sch) 
+	AND slad.WechselNr=999 
+	AND slad.Hochrechnung=0
+	AND sld.Warnung='+'
+	AND sld.Warndatum IS NULL)
