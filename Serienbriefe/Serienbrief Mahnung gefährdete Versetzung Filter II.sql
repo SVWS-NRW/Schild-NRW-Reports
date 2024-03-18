e363bac9ff4c7ffb702cf701a5b9456cf3f6fc1a
Raffenberg <fb.raffenberg@gmail.com>
2024-03-18 10:43:14 +0100

    Angepasster SQL-Befehl für den Mahnbrief für Filter II

--- Serienbriefe/Serienbrief Mahnung gefährdete Versetzung Filter II.sql
+++ Serienbriefe/Serienbrief Mahnung gefährdete Versetzung Filter II.sql
@@ -3,7 +3,7 @@ WHERE Schueler.Status IN (2)
 AND Schueler.Geloescht='-'
 AND Schueler.ASDJahrgang IN ('06','07','08','09','10','EF')
 AND EXISTS (SELECT slad.ID FROM SchuelerLernabschnittsdaten AS slad
-	LEFT JOIN SchuelerLeistungsdaten AS sld ON sld.Abschnitt_ID=slad.ID
+	INNER JOIN SchuelerLeistungsdaten AS sld ON sld.Abschnitt_ID=slad.ID
 	WHERE slad.Schueler_ID=Schueler.ID
 	AND slad.Jahr=(SELECT sch.Schuljahr FROM EigeneSchule AS sch)
 	AND slad.Abschnitt=(SELECT sch.SchuljahrAbschnitt FROM EigeneSchule AS sch) 
